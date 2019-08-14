eff48f11679ea1bd0f8009820decb396bef7591f
shimamotoaoi <>
2019-08-14 15:16:08 +0900

    Laravel-BBS

---
+++ app/Http/Controllers/OppaiController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OppaiController extends Controller
+{
+    public function oppai(){
+      return view('oppai');
+
+    }
+}
